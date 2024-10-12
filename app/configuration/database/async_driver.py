0c4b4b9544bdf1be99e4e5905230703640c6f23b
patacoing <romain.lefebvre1104@gmail.com>
2024-10-12 14:06:54 +0200

    feat: setup db
    We took advantage of DI to build the Database class.
    Unit tests have been written

---
+++ app/configuration/database/async_driver.py
@@ -0,0 +1,33 @@
+from abc import ABC
+from typing import Any
+from neo4j import AsyncGraphDatabase
+
+
+class IAsyncDriver(ABC):
+    def connect(self, uri: str, auth: tuple[str, str]) -> None:
+        ...
+
+    async def close(self) -> None:
+        ...
+
+    async def query(self, query: str, **kwargs: Any) -> Any:
+        ...
+
+
+class AsyncDriver(IAsyncDriver):
+    def connect(self, uri: str, auth: tuple[str, str]) -> None:
+        self.driver = AsyncGraphDatabase.driver(
+            uri,
+            auth=auth
+        )
+
+    async def close(self) -> None:
+        await self.driver.close()
+
+    async def query(self, query: str, **kwargs: Any) -> Any:
+        records, summary, keys = await self.driver.execute_query(query, **kwargs)
+
+        if query.startswith("CREATE"):
+            assert summary.counters.nodes_created > 0
+
+        return records, summary, keys
