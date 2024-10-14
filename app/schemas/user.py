9a9c183bcc1b92bbb4717f15d391b9de9d7a243b
patacoing <romain.lefebvre1104@gmail.com>
2024-10-14 18:54:43 +0200

    feat: user schemas
    We created user schemas that will be used to create and display a user

---
+++ app/schemas/user.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, EmailStr, SecretStr
+
+
+class CreateUserSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: SecretStr
+
+
+class UserSchema(BaseModel):
+    username: str
+    email: EmailStr
