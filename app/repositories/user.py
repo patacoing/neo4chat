bff8b3fa5f891d2603ee37ff39f8510897d5b3ee
patacoing <romain.lefebvre1104@gmail.com>
2024-10-14 20:28:01 +0200

    feat: separated models and schemas

--- app/repositories/user.py
+++ app/repositories/user.py
@@ -2,14 +2,15 @@ from fastapi import Depends
 
 from app.configuration.database.database import Database
 from app.configuration.database.database import get_db
-from app.schemas.user import CreateUserSchema, UserSchema
+from app.models.user import User
+from app.schemas.user import CreateUserSchema
 
 
 class UserRepository:
     def __init__(self, db: Database = Depends(get_db)) -> None:
         self.db = db
 
-    async def create_user(self, user: CreateUserSchema, hashed_password: str) -> UserSchema:
+    async def create_user(self, user: CreateUserSchema, hashed_password: str) -> User:
         await self.db.query(
             "CREATE (u: User {username: $username, email: $email, password: $password})",
             username=user.username,
@@ -17,9 +18,9 @@ class UserRepository:
             password=hashed_password
         )
 
-        return UserSchema(**user.model_dump())
+        return User(**user.model_dump(exclude={"password"}), password=hashed_password)
 
-    async def get_user_by_email(self, email: str) -> UserSchema | None:
+    async def get_user_by_email(self, email: str) -> User | None:
         users, summary, keys = await self.db.query(
             "MATCH (u: User {email: $email}) RETURN u",
             email=email
@@ -28,4 +29,4 @@ class UserRepository:
         if not users:
             return None
 
-        return UserSchema.model_validate(dict(users[0]["u"]))
+        return User.model_validate(dict(users[0]["u"]))
