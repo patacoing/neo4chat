2276b1c5190467c747f37011c76fd2a4c1a6f0f9
patacoing <romain.lefebvre1104@gmail.com>
2024-10-18 13:41:16 +0200

    feat(room): get room by id

--- app/exceptions/room.py
+++ app/exceptions/room.py
@@ -4,3 +4,8 @@ from fastapi import HTTPException, status
 class RoomAlreadyExistsException(HTTPException):
     def __init__(self) -> None:
         super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail="Room already exists")
+
+
+class RoomNotFoundException(HTTPException):
+    def __init__(self) -> None:
+        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail="Room not found")
