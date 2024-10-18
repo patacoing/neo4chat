72cea9d0deacb389b8ba3b34323b11465a716749
patacoing <romain.lefebvre1104@gmail.com>
2024-10-18 23:54:33 +0200

    feat(message): get messages
    We changed the Message model to integrate
    the sent_at and sent_by properties.
    The messages returned are ordered by sent_at

--- app/services/message.py
+++ app/services/message.py
@@ -5,6 +5,7 @@ from app.models.user import User
 from app.repositories.message import MessageRepository
 from app.repositories.room import RoomRepository
 from app.schemas.message import CreateMessageSchema, MessageSchema
+from app.schemas.user import UserSchema
 
 
 class MessageService:
@@ -24,4 +25,25 @@ class MessageService:
 
         message_created = await self.message_repository.create_message(user, room, message)
 
-        return MessageSchema(**message_created.model_dump())
+        return MessageSchema(
+            **message_created.model_dump(exclude={"sent_at", "sent_by"}),
+            sent_at=message_created.sent_at.to_native(),
+            sent_by=UserSchema(**message_created.sent_by.model_dump())
+        )
+
+    async def get_messages_from_room(self, room_id: int) -> list[MessageSchema]:
+        room = await self.room_repository.get_room_by_id(room_id)
+
+        if room is None:
+            raise RoomNotFoundException()
+
+        messages = await self.message_repository.get_messages_from_room_order_by_sent_at(room)
+
+        return [
+            MessageSchema(
+                **message.model_dump(exclude={"sent_at", "sent_by"}),
+                sent_at=message.sent_at.to_native(),
+                sent_by=UserSchema(**message.sent_by.model_dump())
+            )
+            for message in messages
+        ]
