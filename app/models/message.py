e88066d080be6885e1907992b6d9c75939870aab
patacoing <romain.lefebvre1104@gmail.com>
2024-11-08 16:28:32 +0100

    fix: circular imports
    We fixed circular imports from Message and MessageSchema

--- app/models/message.py
+++ app/models/message.py
@@ -2,7 +2,6 @@ from neo4j.time import DateTime
 from pydantic import BaseModel
 
 from app.models.user import UserInMessage
-from app.schemas.message import MessageSchema
 from app.schemas.user import UserSchema
 
 
@@ -16,7 +15,9 @@ class Message(BaseModel):
         "arbitrary_types_allowed": True
     }
 
-    def to_message_schema(self) -> MessageSchema:
+    def to_message_schema(self) -> "MessageSchema":
+        from app.schemas.message import MessageSchema
+
         return MessageSchema(
             **self.model_dump(exclude={"sent_by", "sent_at"}),
             sent_at=self.sent_at.to_native(),
@@ -24,5 +25,5 @@ class Message(BaseModel):
         )
 
     @staticmethod
-    def to_messages_schema(messages: list["Message"]) -> list[MessageSchema]:
+    def to_messages_schema(messages: list["Message"]) -> list["MessageSchema"]:
         return [message.to_message_schema() for message in messages]
--- app/schemas/message.py
+++ app/schemas/message.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+from neo4j.time import DateTime
 from pydantic import BaseModel
 
+from app.models.user import UserInMessage
 from app.schemas.user import UserSchema
 
 
@@ -13,3 +15,16 @@ class MessageSchema(BaseModel):
     content: str
     sent_at: datetime
     sent_by: UserSchema
+
+    def to_message(self) -> "Message":
+        from app.models.message import Message
+
+        return Message(
+            **self.model_dump(exclude={"sent_by", "sent_at"}),
+            sent_by=UserInMessage(**self.sent_by.model_dump()),
+            sent_at=DateTime.fromisoformat(self.sent_at.isoformat())
+        )
+
+    @staticmethod
+    def to_messages(messages: list["MessageSchema"]) -> list["Message"]:
+        return [message.to_message() for message in messages]
