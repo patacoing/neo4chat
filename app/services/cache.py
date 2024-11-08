dc65b99f93e1cdf3762fb7c5e24e5e5e21f3907f
patacoing <romain.lefebvre1104@gmail.com>
2024-11-08 16:28:32 +0100

    fix: circular imports
    We fixed circular imports from Message and MessageSchema

--- app/services/cache.py
+++ app/services/cache.py
@@ -1,11 +1,8 @@
 from fastapi import Depends
-from neo4j.time import DateTime
 
 from app.models.message import Message
-from app.models.user import UserInMessage
 from app.repositories.cache import CacheRepository
 from app.schemas.message import MessageSchema
-from app.schemas.user import UserSchema
 
 
 class CacheService:
@@ -15,23 +12,13 @@ class CacheService:
     def get_messages_order_by_sent_at(self, room_id: int) -> list[MessageSchema]:
         messages = self.cache_repository.get_messages(room_id)
 
-        return sorted([
-            MessageSchema(
-                **message.model_dump(exclude={"sent_at", "sent_by"}),
-                sent_at=message.sent_at.to_native(),
-                sent_by=UserSchema(**message.sent_by.model_dump())
-            )
-            for message in messages
-        ], key=lambda message: message.sent_at, reverse=True)
+        return sorted(
+            Message.to_messages_schema(messages),
+            key=lambda message: message.sent_at,
+            reverse=True
+        )
 
     def set_messages(self, room_id: int, messages_schema: list[MessageSchema]) -> None:
-        messages = [
-            Message(
-                **message.model_dump(exclude={"sent_by", "sent_at"}),
-                sent_by=UserInMessage(**message.sent_by.model_dump()),
-                sent_at=DateTime.fromisoformat(message.sent_at.isoformat())
-            )
-            for message in messages_schema
-        ]
+        messages = MessageSchema.to_messages(messages_schema)
 
         self.cache_repository.set_messages(room_id, messages)
